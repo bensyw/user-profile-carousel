99664fbb4ef0ec8b121754c2ec5f3e269ac7c7e9
Shengyu Wang <shengyuwang95@gmail.com>
2021-02-07 20:08:41 +1100

    Add documentation in PageFooter

--- src/components/PageFooter/PageFooter.tsx
+++ src/components/PageFooter/PageFooter.tsx
@@ -5,6 +5,9 @@ interface PageFooterProps {
     length: number;
 }
 
+/**
+ * Renders the current profile number and the total number of profiles
+ */
 export const PageFooter: React.FunctionComponent<PageFooterProps> = ({ current, length }) => {
     return (
         <div className="page-footer">{`${current + 1} / ${length}`}</div>
