3db3d8fe2b135c85a85d28928824e0ffa720cc56
Shengyu Wang <shengyuwang95@gmail.com>
2021-02-08 10:16:45 +1100

    destructure user object

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -9,27 +9,30 @@ interface ProfileProps {
  * Renders a single user profile
  */
 export const Profile: React.FunctionComponent<ProfileProps> = ({ user }) => {
+    const { name, username, email, address, phone, website, company } = user;
+    const { suite, street, city, zipcode } = address;
+    const { name: companyName, catchPhrase, bs } = company;
     return (
         <>
             <b>User Profile</b>
             <ul>
-                <li> <b>Name: </b> {user.name} </li>
-                <li> <b>Username: </b> {user.username} </li>
-                <li> <b>Email: </b> {user.email} </li>
+                <li> <b>Name: </b> {name} </li>
+                <li> <b>Username: </b> {username} </li>
+                <li> <b>Email: </b> {email} </li>
                 <li> <b>Address: </b>
                     {`
-                        ${user.address.suite}, 
-                        ${user.address.street}, 
-                        ${user.address.city}, 
-                        ${user.address.zipcode}
+                        ${suite}, 
+                        ${street}, 
+                        ${city}, 
+                        ${zipcode}
                         `} </li>
-                <li> <b>Phone: </b> {user.phone} </li>
-                <li> <b>Website: </b> {user.website} </li>
+                <li> <b>Phone: </b> {phone} </li>
+                <li> <b>Website: </b> {website} </li>
                 <li> <b>Company: </b> </li>
                 <ul>
-                    <li> <b>Name: </b> {user.company.name} </li>
-                    <li> <b>Catch Phrase: </b> {user.company.catchPhrase} </li>
-                    <li> <b>Buzzword: </b> {user.company.bs} </li>
+                    <li> <b>Name: </b> {companyName} </li>
+                    <li> <b>Catch Phrase: </b> {catchPhrase} </li>
+                    <li> <b>Buzzword: </b> {bs} </li>
                 </ul>
             </ul>
         </>
