1ea0e2d2db79daf7935d8382a0919947422a2064
Shengyu Wang <shengyuwang95@gmail.com>
2021-02-07 11:58:55 +1100

    Add unit test for PageHeader

---
+++ src/components/PageHeader.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { PageHeader } from './PageHeader';
+
+test('should display header text', () => {
+    render(<PageHeader />);
+    expect(screen.getByText(/CODER ONE/i)).toBeInTheDocument();
+    expect(screen.getByText(/PROGRAMMING TASK/i)).toBeInTheDocument();
+    expect(screen.getByText(/BUILD BY/i)).toBeInTheDocument();
+    expect(screen.getByText(/Shengyu Wang/i)).toBeInTheDocument();
+});
+
+test('should navigate to github page when clicked', () => {
+    render(<PageHeader />);
+    expect(screen.getByText(/Shengyu Wang/i).closest('a')).toHaveAttribute('href', 'https://github.com/bensyw')
+});
